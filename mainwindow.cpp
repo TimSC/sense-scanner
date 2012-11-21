9589df766aa6e1ad064e25f0ae66d8b1e4f2a8d0
Tim Sheerman-Chase <t.sheerman-chase@surrey.ac.uk>
2012-11-21 13:39:03 +0000

    Abstracting the image to a sequence of images

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,31 @@
 #include <assert.h>
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDir>
+
+ImageSequence::ImageSequence(QString targetDir)
+{
+
+}
+
+ImageSequence::~ImageSequence()
+{
+
+}
+
+QSharedPointer<QImage> ImageSequence::Get(long long unsigned ti) //in milliseconds
+{
+    QImage *image = new QImage("/home/tim/dev/QtMedia/test.png");
+    QSharedPointer<QImage> out(image);
+    return out;
+}
+
+long long unsigned ImageSequence::Length() //Get length
+{
+    return 1;
+
+}
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -9,9 +34,11 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
     this->scene = new QGraphicsScene(this);
 
-    QImage image("/home/tim/dev/QtMedia/test.png");
-    assert(!image.isNull());
-    this->item = new QGraphicsPixmapItem(QPixmap::fromImage(image));
+    ImageSequence seq(".");
+    QSharedPointer<QImage> image = seq.Get(0);
+    assert(!image->isNull());
+
+    this->item = new QGraphicsPixmapItem(QPixmap::fromImage(*image));
     this->scene->addItem(item);
     this->ui->graphicsView->setScene(this->scene);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -10,6 +10,15 @@ namespace Ui {
 class MainWindow;
 }
 
+class ImageSequence
+{
+public:
+    ImageSequence(QString targetDir);
+    virtual ~ImageSequence();
+    QSharedPointer<QImage> Get(long long unsigned ti); //in milliseconds
+    long long unsigned Length(); //Get length
+};
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
