dea20697549ea762787d71a5038b5e42a07fde98
TimSC <t.sheerman-chase@surrey.ac.uk>
2012-12-05 16:24:09 +0000

    Surpress console in windows

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <QtGui/QApplication>
 #include "mainwindow.h"
+#include <Windows.h>
 
-int main(int argc, char *argv[])
+int WINAPI WinMain ( HINSTANCE instance, HINSTANCE prev_instance, PSTR cmd_line, int cmd_show )
 {
+	int argc = 0;
+	char **argv = NULL;
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
