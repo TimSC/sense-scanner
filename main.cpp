3e13e48fcc234c5f38a627d37f6c961775355b44
Tim Sheerman-Chase <t.sheerman-chase@surrey.ac.uk>
2012-12-05 22:14:31 +0000

    Make win version work on linux

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <QtGui/QApplication>
 #include "mainwindow.h"
+
+#ifdef _MSC_VER
 #include <Windows.h>
 
 int WINAPI WinMain ( HINSTANCE instance, HINSTANCE prev_instance, PSTR cmd_line, int cmd_show )
 {
-	int argc = 0;
-	char **argv = NULL;
+    int argc = 0;
+    char **argv = NULL;
+#else
+int main(int argc, char *argv[])
+{
+#endif
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
