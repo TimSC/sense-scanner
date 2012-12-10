cc5de4dd65abc7d07d3a23c8547a31041724de20
Tim Sheerman-Chase <t.sheerman-chase@surrey.ac.uk>
2012-12-10 15:34:19 +0000

    Use select to get for stdin

--- echosrv.cpp
+++ echosrv.cpp
@@ -1,28 +1,60 @@
+
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <iostream>
+#include <termios.h>
 using namespace std;
 
-int main(int argc, char *argv[])
-{
-	cout << "Hello world" << endl;
-	char buffer[1000];
+int main(void) {
+    char str[100];
+    fd_set readset;
+    struct timeval tv;
+    struct termios ttystate, ttysave;
 
-	while(1)
-	{
-		usleep(1000000);
+    //get the terminal state
+    tcgetattr(STDIN_FILENO, &ttystate);
+    ttysave = ttystate;
+    //turn off canonical mode and echo
+    ttystate.c_lflag &= ~(ICANON | ECHO);
+    //minimum of number input read.
+    ttystate.c_cc[VMIN] = 1;
 
-		long flags = fcntl(0, F_GETFL); /* get current file status flags */
-		flags |= O_NONBLOCK;		/* turn off blocking flag */
-		fcntl(0, F_SETFL, flags);		/* set up non-blocking read */
+    //set the terminal attributes.
+    tcsetattr(STDIN_FILENO, TCSANOW, &ttystate);
 
-		char* ret = fgets(buffer, 1000, stdin);
+    while(1)
+    {
+		int clearingBuffer = 1;
 
-		cout << "." << (ret == NULL) << endl;
-	}
+		while(clearingBuffer)
+		{
+		    tv.tv_sec = 0;
+		    tv.tv_usec = 1000;
 
-	return 0;
+		    FD_ZERO(&readset);
+		    FD_SET(fileno(stdin), &readset);
+
+		    select(fileno(stdin)+1, &readset, NULL, NULL, &tv);
+		    if(FD_ISSET(fileno(stdin), &readset))
+		    {
+		        cout << (char)fgetc (stdin) << endl;
+				cout.flush();
+		    }
+			else
+				clearingBuffer = 0;
+		}
+
+    }
+
+    ttystate.c_lflag |= ICANON | ECHO;
+    //set the terminal attributes.
+    tcsetattr(STDIN_FILENO, TCSANOW, &ttystate);
+    // report success
+    return 0;
 }
 
