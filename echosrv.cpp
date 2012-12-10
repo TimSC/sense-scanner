89ecdd6af94417048e5b99e34680baeddaad016b
Tim Sheerman-Chase <t.sheerman-chase@surrey.ac.uk>
2012-12-10 19:12:01 +0000

    Reading input can block if we multithread

--- echosrv.cpp
+++ echosrv.cpp
@@ -2,59 +2,23 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/select.h>
-#include <sys/time.h>
-#include <sys/types.h>
+#include <string>
 #include <unistd.h>
-#include <fcntl.h>
-#include <termios.h>
 using namespace std;
 
-int main(void) {
-    char str[100];
-    fd_set readset;
-    struct timeval tv;
-    struct termios ttystate, ttysave;
-
-    //get the terminal state
-    tcgetattr(STDIN_FILENO, &ttystate);
-    ttysave = ttystate;
-    //turn off canonical mode and echo
-    ttystate.c_lflag &= ~(ICANON | ECHO);
-    //minimum of number input read.
-    ttystate.c_cc[VMIN] = 1;
-
-    //set the terminal attributes.
-    tcsetattr(STDIN_FILENO, TCSANOW, &ttystate);
-
-    while(1)
-    {
-		int clearingBuffer = 1;
-
-		while(clearingBuffer)
-		{
-		    tv.tv_sec = 0;
-		    tv.tv_usec = 1000;
-
-		    FD_ZERO(&readset);
-		    FD_SET(fileno(stdin), &readset);
-
-		    select(fileno(stdin)+1, &readset, NULL, NULL, &tv);
-		    if(FD_ISSET(fileno(stdin), &readset))
-		    {
-		        cout << (char)fgetc (stdin) << endl;
-				cout.flush();
-		    }
-			else
-				clearingBuffer = 0;
-		}
-
-    }
-
-    ttystate.c_lflag |= ICANON | ECHO;
-    //set the terminal attributes.
-    tcsetattr(STDIN_FILENO, TCSANOW, &ttystate);
-    // report success
-    return 0;
+int main(int argc, char *argv[])
+{
+	int running = 1;
+	while(running)
+	{
+		std::string mystring;
+		cin >> mystring;
+		//cout << mystring << mystring.length() << endl;
+		if(mystring == "QUIT")
+			running = 0;
+		if(mystring == "GET_PROGRESS")
+			cout << "PROGRESS=0.5" << endl;
+	}
+	cout << "FINISHED" << endl;
 }
 
