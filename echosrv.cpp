85c1dbc8b686fc78d07f7f6c24a3956e4653342d
Tim Sheerman-Chase <t.sheerman-chase@surrey.ac.uk>
2012-12-10 19:30:19 +0000

    Making worker thread in separate process

--- echosrv.cpp
+++ echosrv.cpp
@@ -1,24 +1,62 @@
-
+//g++ -std=c++0x -pthread echosrv.cpp -o echosrv
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
+#include <thread>
+#include <mutex>
 #include <unistd.h>
 using namespace std;
 
+int gRunning = 1;
+std::mutex gRunningMutex;
+
+class Worker
+{
+public:
+    void operator()() const
+    {
+		gRunningMutex.lock();
+		int running = gRunning;
+		gRunningMutex.unlock();
+		while(running)
+		{
+        	cout << "PROGRESS=0.5" << endl;
+			usleep(1000000);
+
+			gRunningMutex.lock();
+			running = gRunning;
+			gRunningMutex.unlock();
+		}
+		
+    }
+};
+
 int main(int argc, char *argv[])
 {
-	int running = 1;
+	std::thread thr((Worker()));
+	gRunningMutex.lock();
+	int running = gRunning;
+	gRunningMutex.unlock();
+
 	while(running)
 	{
+		//Read standard input
 		std::string mystring;
 		cin >> mystring;
-		//cout << mystring << mystring.length() << endl;
+
+		//Process commands
 		if(mystring == "QUIT")
-			running = 0;
-		if(mystring == "GET_PROGRESS")
-			cout << "PROGRESS=0.5" << endl;
+			gRunning = 0;
+		//if(mystring == "GET_PROGRESS")
+		//	cout << "PROGRESS=0.5" << endl;
+
+		gRunningMutex.lock();
+		running = gRunning;
+		gRunningMutex.unlock();
 	}
+	
+	thr.join();
 	cout << "FINISHED" << endl;
 }
 
