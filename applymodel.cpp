b35f37e687e31aa647f3c17982b3c7febe8db656
TimSC <tim2009@sheerman-chase.org.uk>
2013-03-11 11:12:55 +0000

    Remove unused code

--- applymodel.cpp
+++ applymodel.cpp
@@ -111,39 +111,6 @@ void ApplyModel::Update()
         }
     }
 
-    /*
-
-    //Check if this thread should be active and access the video
-    assert(this->parentAnn != NULL);
-    QUuid algUid = this->parentAnn->GetAlgUid();
-
-    QString src = this->parentAnn->GetSource();
-    int activeStateDesired = this->parentAnn->GetActiveStateDesired();
-    if(!activeStateDesired)
-    {
-        this->msleep(1);
-        return;
-    }
-
-    if(algUid.isNull())
-    {
-        //cout << this->parentAnn->GetSource().toLocal8Bit().constData() << endl;
-        //cout << algUid.toString().toLocal8Bit().constData() << endl;
-        if(this->parentAnn->GetActive())
-            this->parentAnn->SetActiveStateDesired(0);
-        else
-            this->msleep(1);
-        return;
-    }
-
-    int isActive = this->parentAnn->GetActive();
-    if(!isActive)
-    {
-        this->msleep(1);
-        return;
-    }
-*/
-
     if(!this->srcDurationSet)
     {
         this->srcDuration = AvBinMedia::GetMediaDuration(this->srcFina,
@@ -155,49 +122,13 @@ void ApplyModel::Update()
         return;
     }
 
-/*
-    //Get list of avilable frames
-    if(!this->frameTimesSet)
-    {
-        track->GetFramesAvailable(this->frameTimes, this->frameTimesEnd);
-        this->frameTimesSet = true;
-        this->msleep(1);
-        return;
-    }
-*/
     unsigned long long frameDuration = 0; //millisec
     unsigned long long avTi = 0; //millisec
     unsigned long long nextTi = 0; //millisec
 
     //Check algorithm is ready to work
     //TODO
-/*
-    //WHAT is this for? Getting the start frame time from cache?
-    if(this->frameTimesSet && this->currentTimeSet==false && this->frameTimes.size() > 0)
-    {
-        std::map<unsigned long, unsigned long>::iterator it = this->frameTimes.begin();
-        assert(it!=this->frameTimes.end());
-        unsigned long start = it->first;
-        unsigned long end = it->second;
-        avTi = (unsigned long long)(0.5 * (start + end) + 0.5); //millisec
 
-        std::vector<std::vector<float> > foundAnnot;
-        unsigned long long foundAnnotationTime=0;
-        int found = track->GetAnnotationBetweenTimestamps(start,
-                                              end,
-                                              avTi,
-                                              foundAnnot,
-                                              foundAnnotationTime);
-
-        //Check if first frame has already been annotated
-        if(found)
-        {
-            this->currentTimeSet = true;
-            this->currentStartTimestamp = start;
-            this->currentEndTimestamp = end;
-        }
-    }
-*/
     //If needed, get the first frame from the video
     if(this->currentTimeSet==false)
     {
@@ -267,87 +198,13 @@ void ApplyModel::Update()
         this->msleep(1);
         return;
     }
-/*
-    //If the list of frames has not been set, assume it is blank
-    this->frameTimesSet = true;
-*/
+
     //Estimate mid time of next frame
     assert(this->currentTimeSet==true);
     frameDuration = this->currentEndTimestamp - this->currentStartTimestamp; //millisec
     avTi = (unsigned long long)(0.5 * (this->currentStartTimestamp + this->currentEndTimestamp) + 0.5); //millisec
     nextTi = avTi + frameDuration; //millisec
     assert(nextTi > 0);
-/*
-    //Check if known frames can satisfy iterations
-    int knownFrame = 1;
-    int countKnown = 0;
-    if(nextTi < this->srcDuration) while(knownFrame)
-    {
-        unsigned long long milsec = nextTi;
-
-        //Check if the next frame start and end is already known
-        std::map<unsigned long, unsigned long>::iterator fit = this->frameTimes.find(this->currentEndTimestamp);
-        if(fit != this->frameTimes.end())
-        {
-            //Frame duration is already known
-            //Now check if annotation already exists
-            std::vector<std::vector<float> > foundAnnot;
-            unsigned long long foundAnnotationTime;
-
-            //Get frame at expected time (fast)
-            int found = track->GetAnnotationAtTime(milsec,
-                                                  foundAnnot);
-            if(found)
-            {
-                foundAnnotationTime = milsec;
-            }
-            else
-            {
-                //Get frames anywhere in frame iterval (slow)
-                found = track->GetAnnotationBetweenTimestamps(
-                    this->currentStartTimestamp,
-                    this->currentEndTimestamp,
-                    milsec,
-                    foundAnnot,
-                    foundAnnotationTime);
-            }
-
-            if(found)
-            {
-                //Update current model from annotation
-                this->currentModel = foundAnnot;
-                this->currentModelSet = 1;
-                countKnown ++;
-
-                //This frame is done, go to next frame
-                this->currentStartTimestamp = fit->first;
-                this->currentEndTimestamp = fit->second;
-                frameDuration = this->currentEndTimestamp - this->currentStartTimestamp;
-                avTi = (unsigned long long)(0.5 * (this->currentStartTimestamp + this->currentEndTimestamp) + 0.5);
-                nextTi = avTi + frameDuration;
-            }
-            else
-                knownFrame = 0;
-
-            //After 1 frame, return to allow thread messages to be processed
-            if(countKnown>0)
-            {
-                //Estimate progress and generate an event
-                double progress = double(milsec) / this->srcDuration;
-                std::tr1::shared_ptr<class Event> requestEv(new Event("THREAD_PROGRESS_UPDATE"));
-                requestEv->fromUuid = this->parentAnn->GetAnnotUid();
-                QString progressStr = QString("%1").arg(progress);
-                requestEv->data = progressStr.toLocal8Bit().constData();
-                this->eventLoop->SendEvent(requestEv);
-                this->msleep(1);
-                return;
-            }
-
-        }
-        else
-            knownFrame = 0;
-    }
-*/
 
     //Get subsequent frames
     if(nextTi < this->srcDuration)
