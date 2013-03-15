a182a6346a27dc4c12964dd794e103429a7e758e
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2013-03-15 15:00:08 +0000

    Remove beta 1 version info and merge back into master

--- version.h
+++ version.h
@@ -1,9 +1,9 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION_STR "v1.0 Beta 1"
-#define VERSION_URL "v1beta1"
+#define VERSION_STR "dev-postb1"
+#define VERSION_URL "dev-postb1"
 
-#define RELEASE_MODE
+//#define RELEASE_MODE
 
 #endif // VERSION_H
